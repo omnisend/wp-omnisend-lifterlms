c794a076bd4f877f0153109d4f9a7ec30e5223f8
Greta Miknevičiūtė <greta@omnisend.com>
2024-09-11 11:13:44 +0300

    add all the basics

---
+++ lint.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+alias composer='docker run --rm -it -v "$(pwd):/app" composer/composer'
+
+action=$1
+
+case $action in
+"check")
+  composer update
+  composer install
+  ./vendor/bin/phpcs -s omnisend-for-lifterlms
+  ;;
+"fix")
+  composer update
+  composer install
+  ./vendor/bin/phpcbf omnisend-for-lifterlms
+  ;;
+*)
+  echo "pass one of these argument: check,fix"
+  exit 1
+  ;;
+esac
+.\
